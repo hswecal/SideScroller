c6de59731669011141cf5edbdb73e654d937e3e4
max burger <max.johanssonkinsella@elev.ga.lbs.se>
2022-12-14 10:03:33 +0100

    fiender

---
+++ Assets/enemyScrpt.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyScrpt : MonoBehaviour
+{
+    //kod för fiender, rörelse - max
+
+    bool movingLeft = true;
+    Vector3 targetPosLeft = new Vector3();
+    Vector3 targetPosRight = new Vector3();
+
+
+
+    void Start()
+    {
+        targetPosLeft.x = transform.position.x - 5;
+        targetPosRight.x = transform.position.x + 5;
+    }
+
+
+    void Update()
+    {
+        if (movingLeft == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosLeft, 1 * Time.deltaTime); //fiende rör sig åt vänster - max
+            if (transform.position == targetPosLeft)
+            {
+                movingLeft = false;
+            }
+        }
+
+        if (movingLeft == false)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosRight, 1 * Time.deltaTime); //fiende rör sig åt höger - max
+            if (transform.position == targetPosRight)
+            {
+                movingLeft = true;
+            }
+        }
+    }
+}
