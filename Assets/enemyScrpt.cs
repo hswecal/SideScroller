cd3b824c33d9ae229c36ce2a61a59153794ee8ab
max burger <max.johanssonkinsella@elev.ga.lbs.se>
2022-12-16 09:26:32 +0100

    försöker göra attack kod

--- Assets/enemyScrpt.cs
+++ Assets/enemyScrpt.cs
@@ -4,24 +4,34 @@ using UnityEngine;
 
 public class enemyScrpt : MonoBehaviour
 {
-    //kod för fiender, rörelse - max
+    //kod för fiender, rörelse, död - max
 
     bool movingLeft = true;
     Vector3 targetPosLeft = new Vector3();
     Vector3 targetPosRight = new Vector3();
-  
+    float attackTimer = 0f;
+    float stopwatch = 0f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject); //dör om den rör en spelar - max
+        }
+    }
 
     void Start()
     {
         targetPosLeft.x = transform.position.x - 5;
         targetPosRight.x = transform.position.x + 5;
-        
+        attackTimer = Random.Range(3, 8);
 
     }
 
 
     void Update()
     {
+        stopwatch += Time.deltaTime;
         if (movingLeft == true)
         {
             transform.position = Vector3.MoveTowards(transform.position, targetPosLeft, 1 * Time.deltaTime); //fiende rör sig åt vänster - max
@@ -39,5 +49,11 @@ public class enemyScrpt : MonoBehaviour
                 movingLeft = true;
             }
         }
+
+        if(stopwatch >= attackTimer)
+        {
+
+        }
+
     }
 }
