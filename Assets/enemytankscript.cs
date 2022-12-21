73165ddf2fa2a09c1016240cf1ae87a1c339e96b
max burger <max.johanssonkinsella@elev.ga.lbs.se>
2022-12-21 08:54:25 +0100

    hej casper

--- Assets/enemytankscript.cs
+++ Assets/enemytankscript.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class enemytankscript : MonoBehaviour
 {
     float stopwatch = 0f;
-    public GameObject bullet;
+    public GameObject bulletPrefab;
+    Vector3 force = new Vector3(-1, 0);
 
     // Start is called before the first frame update
     void Start()
@@ -19,8 +20,9 @@ public class enemytankscript : MonoBehaviour
         stopwatch += Time.deltaTime;
         if(stopwatch >= 5)
         {
-            Instantiate(bullet, transform.position, transform.rotation);
-            bullet.rb2D.addForce
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            bullet.GetComponent<Rigidbody2D>().AddForce(force * 5);
+            stopwatch = 0;
         }
     }
 }
