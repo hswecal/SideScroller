df596a13b231e4ba8448926769a5d46a2d90f282
hswecal <hswecal@gmail.com>
2022-12-21 11:02:56 +0100

    Update enemytankscript.cs

--- Assets/enemytankscript.cs
+++ Assets/enemytankscript.cs
@@ -19,12 +19,6 @@ public class enemytankscript : MonoBehaviour
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
